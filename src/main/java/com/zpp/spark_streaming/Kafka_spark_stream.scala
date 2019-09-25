d259b4074184052ad7959f698544147246a8628c
zpp7123 <Zhang1988>
2019-09-25 17:30:02 +0800

    update cao

--- src/main/java/com/zpp/spark_streaming/Kafka_spark_stream.scala
+++ src/main/java/com/zpp/spark_streaming/Kafka_spark_stream.scala
@@ -9,7 +9,7 @@ object Kafka_spark_stream {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("word count")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val ssc = new StreamingContext(conf, Seconds(10))
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "master:9092,slave1:9092",
       "key.deserializer" -> classOf[StringDeserializer],
@@ -24,7 +24,10 @@ object Kafka_spark_stream {
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
-    stream.map(record => (record.key, record.value,record.timestamp())).print()
+    // 统计瞬间data信息
+    stream.map(record => (record.key, record.value,record.timestamp())).print(10)
+    // 时间窗口统计value个数
+    stream.map(record => (record.value, 1)).reduceByKeyAndWindow((a: Int, b: Int) => (a + b), Seconds(40), Seconds(20)).print(10)
 
     ssc.start()
     ssc.awaitTermination()
